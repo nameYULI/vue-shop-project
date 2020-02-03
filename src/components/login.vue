3cf368647e0fa65049b114cb7372b53264d60abf
于莉 <1922708088@qq.com>
2020-02-04 01:20:11 +0800

    登陆页面创建

---
+++ src/components/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login_container">登陆组件</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+}
+</style>
