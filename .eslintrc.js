ec0b05fbc8123a642fd1163185101dd09d791e2a
于莉 <1922708088@qq.com>
2020-02-04 00:53:22 +0800

    清理默认项目的一些东西

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,11 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    //强制使用单引号
+    quotes: ['error', 'single'],
+    //强制不使用分号结尾
+    semi: ['error', 'never']
   },
   parserOptions: {
     parser: "babel-eslint"
