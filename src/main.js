6002f79813ce339fd0af0adb447fd3879a00cc90
于莉 <1922708088@qq.com>
2020-02-04 15:47:29 +0800

    引入axios

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
 import './plugins/element.js'
+import'./assets/css/global.css'
 
-Vue.config.productionTip = false;
+import axios from 'axios'
+axios.defaults.baseURL='https://www.liulongbin.top:8888/api/private/v1'
+Vue.prototype.$http=axios
+
+Vue.config.productionTip = false
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
