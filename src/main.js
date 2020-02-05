f60e01c6b1e6bf32e369621b58e9b4c891f0298d
于莉 <1922708088@qq.com>
2020-02-05 18:58:53 +0800

    home页面完善以及各子页面创建

--- src/main.js
+++ src/main.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
-import'./assets/css/global.css'
+import './assets/css/global.css'
 
 import axios from 'axios'
-axios.defaults.baseURL='https://www.liulongbin.top:8888/api/private/v1'
-Vue.prototype.$http=axios
+axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1'
+axios.interceptors.request.use(config => {
+  // console.log(config)
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  //在最后必须return config
+  return config
+})
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,37 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/login.vue'
 import Home from '../components/home.vue'
+import Welcome from '../components/welcome.vue'
+import Users from '../components/user/users.vue'
+import Roles from '../components/role/roles.vue'
+import Rights from '../components/role/rights.vue'
+import Orders from '../components/order/orders.vue'
+import Goods from '../components/goods/goods.vue'
+import Params from '../components/goods/params.vue'
+import Categories from '../components/goods/categories.vue'
+import Reports from '../components/data/reports.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  { path:'/', redirect:'/login'},
-  { path:'/login', component: Login},
-  { path:'/home', component: Home}
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  {
+    path: '/home',
+    component: Home,
+    redirect: '/welcome',
+    children: [
+      { path: '/welcome', component: Welcome },
+      { path: '/users', component: Users },
+      { path: '/roles', component: Roles },
+      { path: '/rights', component: Rights },
+      { path: '/orders', component: Orders },
+      { path: '/goods', component: Goods },
+      { path: '/params', component: Params },
+      { path: '/categories', component: Categories },
+      { path: '/reports', component: Reports }
+    ]
+  }
 ]
 
 const router = new VueRouter({
@@ -16,15 +40,15 @@ const router = new VueRouter({
 })
 
 //挂载路由导航守卫
-router.beforeEach((to,from,next)=>{
+router.beforeEach((to, from, next) => {
   //to代表将要访问的路径
   //from代表从哪个路经跳转而来
   //next是一个函数表示放行
   // next() 放行   next('/login') 强制跳转
-  if(to.path==='/login') return next()
+  if (to.path === '/login') return next()
   // 获取token
-  const tokenStr= window.sessionStorage.getItem('token')
-  if(!tokenStr) return next('/login') //未登录状态强制跳转
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login') //未登录状态强制跳转
   next()
 })
 
