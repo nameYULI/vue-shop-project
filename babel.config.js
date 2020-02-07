95aad99fc90b921ea77a41d9d7d0b15eec198012
于莉 <1922708088@qq.com>
2020-02-08 01:49:32 +0800

    修改设置只在发部阶段移除所有的console

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
+//这是项目发部阶段需要用到的babel插件
+const prodPlugins = []
+if (process.env.NODE_ENV === 'production') {
+  prodPlugins.push('transform-remove-console')
+}
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
   plugins: [
@@ -8,6 +13,7 @@ module.exports = {
         styleLibraryName: 'theme-chalk'
       }
     ],
-    'transform-remove-console'
+    //发布产品时候的插件数组
+    ...prodPlugins
   ]
 }
